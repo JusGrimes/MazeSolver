003192da6a9d9811a52015ef24fe3831bea8edc2
Justin Grimes <jusgrimes@gmail.com>
2019-07-19 20:27:12 -0500

    made MazeFactory

---
+++ src/com/justin/MazeFactory.java
@@ -0,0 +1,21 @@
+package com.justin;
+
+import org.jetbrains.annotations.Contract;
+
+public class MazeFactory {
+    private MazeFactory() {
+
+    }
+
+    static public Maze openMaze(int rows, int cols){
+        return new Maze(rows, cols, 0, 0, rows - 1, cols - 1, 1);
+    }
+
+    static public Maze closedMaze(int rows, int cols) {
+        return new Maze(rows, cols, 0, 0, rows - 1, cols - 1, 0);
+    }
+
+    static public Maze standardMaze(int rows, int cols, double density) {
+        return new Maze(rows, cols, 0, 0, rows - 1, cols - 1, density);
+    }
+}
