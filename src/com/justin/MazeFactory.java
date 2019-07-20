dc1a1828dba1273ab81b111072a5c88792c85ac0
Justin Grimes <jusgrimes@gmail.com>
2019-07-19 20:46:42 -0500

    fixed openMaze and closedMaze

--- src/com/justin/MazeFactory.java
+++ src/com/justin/MazeFactory.java
@@ -8,11 +8,11 @@ public class MazeFactory {
     }
 
     static public Maze openMaze(int rows, int cols){
-        return new Maze(rows, cols, 0, 0, rows - 1, cols - 1, 1);
+        return new Maze(rows, cols, 0, 0, rows - 1, cols - 1, 0);
     }
 
     static public Maze closedMaze(int rows, int cols) {
-        return new Maze(rows, cols, 0, 0, rows - 1, cols - 1, 0);
+        return new Maze(rows, cols, 0, 0, rows - 1, cols - 1, 1);
     }
 
     static public Maze standardMaze(int rows, int cols, double density) {
