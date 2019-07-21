0ad5d79e355ec7f248051fc6d0e8261648b81c9b
Justin Grimes <jusgrimes@gmail.com>
2019-07-21 15:29:11 -0500

    added getters for goal and start

--- src/com/justin/Node.java
+++ src/com/justin/Node.java
@@ -3,19 +3,19 @@ package com.justin;
 public class Node {
 
     private Node parent;
-    private State state;
+    private Location location;
     private double cost;
     private double heuristic;
 
-    public Node(Node parent, State state, double cost, double heuristic) {
+    Node(Node parent, Location location, double cost, double heuristic) {
         this.parent = parent;
-        this.state = state;
+        this.location = location;
         this.cost = cost;
         this.heuristic = heuristic;
     }
 
-    public State getState() {
-        return state;
+    Location getLocation() {
+        return location;
     }
 
     public Node getParent() {
